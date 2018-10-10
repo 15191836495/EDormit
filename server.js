f4a593b04b0c6ebf1cc576946973b4659882a7ea
pipipapi <1638464457@qq.com>
2018-10-10 08:55:21 +0800

    check mode
    10/10 8:55

--- server.js
+++ server.js
@@ -9,9 +9,19 @@ let server = net.createServer(function(socket){
     console.log('Server：start to listen');
 // 接收数据并处理   处理nodemcu数据在这里
     socket.on(`data`, function(data){
+                                       // 转字符串输出控制台
+        let data1 = data.toString();
+        let data2 = JSON.parse(data1);
+        console.log(data2.M);
+        if (data2.M === "checkin"){
+            socket.write('commend checin recieve \n');
+        }
+        else {
+            //断开连接
+        }
 
-        console.log(data.toString());
 //接收数据返回字符串 发送操作在这里
+        //收到web发来的json数据
         socket.write('recieve \n');
     });
 

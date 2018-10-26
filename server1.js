8f428bdda29f33467b260c4452d5ec336429361c
pipipapi <1638464457@qq.com>
2018-10-26 20:09:16 +0800

    debug
    debug

--- server1.js
+++ server1.js
@@ -1,17 +1,42 @@
+//读取文件时必须经过两步操作
 var http = require('http');
 var fs = require("fs");
+var querystring = require('querystring');
 var server = http.createServer(function (req, res) {
     var body = '';
-    req.on('data', function(data) {      // 接收客户端发送过来的数据， 也就是 xmlHttp.send(value);
-        body += data;
-        console.log(body);
+    req.on('data', function(data) {      
+        body += data;        
+        var data1 = data.toString();
+        var data2 = querystring.parse(data1);
+        console.log(data2);
+ 
+        if(data2.M === 'mcustaus')  {
+            var data5=JSON.stringify(data2);
+           fs.writeFile('mcustaus.json',data5,'utf8',function(err) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log('硬件状态已更新'); })
+            
+        } 
+        else if (data2.M ==='com'){
+            var data4=JSON.stringify(data2);
+            fs.writeFile('com.json',data4,function(err) {
+                if (err) {
+                    return console.error(err);
+                }
+                console.log('前端数据已收到命令文件已经更改'); }) 
+                
+        
         res.writeHeader(200, {
             'Content-Type': 'text/plain',
-            'Access-Control-Allow-Origin': '*'    //解决跨域问题
+            'Access-Control-Allow-Origin': '*'    
         });
-        res.write('the commend is sending');
+        res.write(' 命令已收到');
         res.end();
+    }
     });
+    
 
     
 });
